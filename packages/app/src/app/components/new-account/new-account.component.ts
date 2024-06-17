dd4f4dd5175d05267b6073450d7cf607b31dadf8
alanortega-uno <alan.ortega.mh@gmail.com>
2024-06-17 11:12:43 -0400

    [FE] Refactor: SCSS

--- packages/app/src/app/components/new-account/new-account.component.ts
+++ packages/app/src/app/components/new-account/new-account.component.ts
@@ -8,7 +8,6 @@ import { ValidationService } from 'src/app/services/validation.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.scss'],
 })
 export class NewAccountComponent implements OnInit {
   newAccountForm = this.formBuilder.group({
