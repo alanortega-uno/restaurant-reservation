93d332004b509df82889f961b71f045318ec035d
alanortega-uno <alan.ortega.mh@gmail.com>
2024-06-20 15:02:24 -0400

    [Shared] ApiRequestStatus enum

---
+++ packages/shared/enums/api-request-status.ts
@@ -0,0 +1,6 @@
+export enum ApiRequestStatus {
+  pending = "pending",
+  loading = "loading",
+  error = "error",
+  success = "success",
+}
--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,4 +1,5 @@
 import { User } from "./interfaces/users";
 import { APIError, ApiError } from "./interfaces/errors";
+import { ApiRequestStatus } from "./enums/api-request-status";
 
-export { User, APIError, ApiError };
+export { User, APIError, ApiError, ApiRequestStatus };
