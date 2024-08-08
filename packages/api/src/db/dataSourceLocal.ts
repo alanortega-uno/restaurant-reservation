4592c4ecaba96dd1f21742504e42ad3f5569305e
alanortega-uno <alan.ortega.mh@gmail.com>
2024-08-08 15:22:00 -0400

    [BE] Build problems Fixed

--- packages/api/src/db/dataSourceLocal.ts
+++ packages/api/src/db/dataSourceLocal.ts
@@ -2,6 +2,11 @@ import { DataSource } from "typeorm";
 import { DataSourceOptions } from "typeorm/data-source/DataSourceOptions";
 
 import dotenv from "dotenv";
+import { AccountEntity } from "../entities/account";
+import { RefreshTokenEntity } from "../entities/refreshToken";
+import { ReservationEntity } from "../entities/reservation";
+import { TableEntity } from "../entities/table";
+import { InitialMigration1722439928348 } from "./migrations/1722439928348-initial-migration";
 let envFileName = ".env";
 if (process.env.NODE_ENV === "e2e") envFileName = `.env.e2e`;
 
@@ -16,8 +21,8 @@ let connectionOptions: DataSourceOptions = {
   database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
-  entities: ["src/entities/*{.ts,.js}"],
-  migrations: ["src/db/migrations/*{.ts,.js}"],
+  entities: [AccountEntity, RefreshTokenEntity, ReservationEntity, TableEntity],
+  migrations: [InitialMigration1722439928348],
 };
 
 export default new DataSource({
